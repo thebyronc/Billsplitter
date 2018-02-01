304af03b5efe845ad02e5ba027bb7eb0aa735d57
Edgar Hernandez and Kate Trahan <edgar@>
2018-02-01 08:28:53 -0800

    Add click function in frontend.js

--- html/js/frontend.js
+++ html/js/frontend.js
@@ -8,7 +8,7 @@ var getAllUsers = function() {
         },
         success: function(response) {
             for(i = 0; i < response.length; i++ ){
-                $('#allUsers').prepend(`<li class="list-group-item"><span class="userName">USER:</span> ${response[i].name} <br> <span class="userName">EMAIL:</span> ${response[i].email} <br><span class="userName">ID:</span> ${response[i].id}<span class="deleteId">Delete Id: <a href="#" onclick="deleteById(${response[i].id})"> Delete User</a></span></li>`);
+                $('#allUsers').prepend(`<li class="list-group-item"><span class="userName">USER:</span> ${response[i].name} <br> <span class="userName">EMAIL:</span> ${response[i].email} <br><span class="userName">ID:</span> ${response[i].id}<span class="deleteId">Delete Id: <a href="#" onclick="currentUser(${response[i].id})"> Delete User</a></span></li>`);
             }
         },
         error: function(){
@@ -27,9 +27,20 @@ var clearAll = function() {
         },
     });
 }
+var deleteUser = function() {
+    $('#currentUser').html('');// Need to connect this to table
+    $.ajax({
+        url: "http://localhost:4567/users/:userId/delete",
+        type: 'GET',
+        date: {
+        format: 'json'
+        },
+    });
+    }
 
 $(document).ready(function() {
     getAllUsers();
+    deleteUser();
 
     $("#addUser").submit(function(event) {
     event.preventDefault();
@@ -62,7 +73,26 @@ $(document).ready(function() {
     });
     clearAll();
     });
-});
+
+    $("#currentUser").click(function(event) {
+    event.preventDefault();
+    var id = $("userId").val();
+    var personId = {
+    "id" : id
+    };
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:4567/users/:userId/delete",
+        data: JSON.stringify(),
+        dataType: "json",
+        success: function(){},
+        failure: function(errMsg){
+            console.log("Error can't delete");
+        }
+        });
+        getAllUsers();
+    });
+    }
 
 
 
